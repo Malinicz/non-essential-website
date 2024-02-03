26246658d1bc3ff9390942c45ff59d5488be9bdd
Malinicz <malinicz@gmail.com>
2024-02-03 16:35:50 +0100

    update locale switcher

--- src/app/[lang]/components/LocaleSwitcher/LocaleSwitcher.tsx
+++ src/app/[lang]/components/LocaleSwitcher/LocaleSwitcher.tsx
@@ -1,16 +1,17 @@
 "use client";
 
+import cx from "classnames";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { i18n } from "@/i18n-config";
 import styles from "./LocaleSwitcher.module.scss";
 
 export function LocaleSwitcher() {
-  const pathName = usePathname();
+  const pathname = usePathname();
 
   const redirectedPathName = (locale: string) => {
-    if (!pathName) return "/";
-    const segments = pathName.split("/");
+    if (!pathname) return "/";
+    const segments = pathname.split("/");
     segments[1] = locale;
     return segments.join("/");
   };
@@ -18,9 +19,16 @@ export function LocaleSwitcher() {
   return (
     <ul className={styles.list}>
       {i18n.locales.map((locale) => {
+        const isActive = pathname.startsWith(`/${locale}/`);
+
         return (
           <li key={locale} className={styles.listItem}>
-            <Link href={redirectedPathName(locale)}>{locale}</Link>
+            <Link
+              href={redirectedPathName(locale)}
+              className={cx({ [styles.activeLocale]: isActive })}
+            >
+              {locale}
+            </Link>
           </li>
         );
       })}
