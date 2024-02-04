3ebcf653465e33764c63bf4f58198a838639f682
Malinicz <malinicz@gmail.com>
2024-02-04 20:42:14 +0100

    create music section

--- src/app/[lang]/_components/BandCampPlugin/BandCampPlugin.tsx
+++ src/app/[lang]/_components/BandCampPlugin/BandCampPlugin.tsx
@@ -1,21 +1,28 @@
+import cx from "classnames";
 import styles from "./BandCampPlugin.module.scss";
 
-const ALBUM_URL = {
-  impossible:
-    "https://bandcamp.com/EmbeddedPlayer/track=2895797269/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/",
-  fire: "https://bandcamp.com/EmbeddedPlayer/track=2836393470/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/",
-  people:
-    "https://bandcamp.com/EmbeddedPlayer/track=1168130426/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/",
-};
-
 type PropsType = Readonly<{
-  album: keyof typeof ALBUM_URL;
+  album: "impossible" | "fire" | "people";
+  size?: "small" | "large";
 }>;
 
-export function BandCampPlugin({ album }: PropsType) {
+export function BandCampPlugin({ album, size }: PropsType) {
   if (!album) return null;
 
+  const albums = {
+    impossible: `https://bandcamp.com/EmbeddedPlayer/track=2895797269/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=${size}/transparent=true/`,
+    fire: `https://bandcamp.com/EmbeddedPlayer/track=2836393470/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=${size}/transparent=true/`,
+    people: `https://bandcamp.com/EmbeddedPlayer/track=1168130426/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=${size}/transparent=true/`,
+  };
+
   return (
-    <iframe className={styles.container} src={ALBUM_URL[album]} seamless />
+    <iframe
+      className={cx({
+        [styles.containerLarge]: size === "large",
+        [styles.containerSmall]: size === "small",
+      })}
+      src={albums[album]}
+      seamless
+    />
   );
 }
--- src/app/[lang]/music/page.tsx
+++ src/app/[lang]/music/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import cx from "classnames";
 import { getDictionary } from "@/get-dictionary";
 import { Locale } from "@/i18n-config";
+import { BandCampPlugin } from "../_components";
+import styles from "./page.module.scss";
 
 export default async function Page({
   params: { lang },
@@ -9,5 +12,28 @@ export default async function Page({
 }) {
   const dictionary = await getDictionary(lang);
 
-  return <div>music</div>;
+  return (
+    <div className="gap-y-m">
+      <div>
+        <h1>{dictionary.music.heading}</h1>
+        <h2>{dictionary.music.singles.heading}</h2>
+      </div>
+      <div className={styles.albums}>
+        <div className={cx(styles.bandCampPluginSmall, "gap-y-s")}>
+          {["impossible" as const, "fire" as const, "people" as const].map(
+            (name) => (
+              <BandCampPlugin key={name} album={name} size="small" />
+            )
+          )}
+        </div>
+        <div className={cx(styles.bandCampPluginLarge, "gap-y-m")}>
+          {["impossible" as const, "fire" as const, "people" as const].map(
+            (name) => (
+              <BandCampPlugin key={name} album={name} size="large" />
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
